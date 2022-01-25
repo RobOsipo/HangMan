fc9a5469537082d2a7b55934d3610d124a2d6200
HartMason <hartmason85@gmail.com>
2022-01-24 20:21:20 -0600

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+console.log("hartsHangMan")
 // brings in the assert module for unit testing
 const assert = require('assert');
 // brings in the readline module to access the command line
